28cd5c60afa4511538bdca8ba44211fe4c850db8
SARTH NAROLA <168976016+Sarth00718@users.noreply.github.com>
2025-07-13 16:57:22 +0530

    Update usercontroller.js

--- backend/controllers/usercontroller.js
+++ backend/controllers/usercontroller.js
@@ -77,7 +77,7 @@ export const login = async (req, res) => {
 
         const token = await jwt.sign(tokenData, process.env.JWT_SECRET, { expiresIn: '1d' });
 
-        return res.status(200).cookie("token", token, { maxAge: 1 * 24 * 60 * 60 * 1000, httpOnly: true, sameSite: 'None' }).json({
+        return res.status(200).cookie("token", token, { maxAge: 1 * 24 * 60 * 60 * 1000, httpOnly: true, sameSite: 'None', secure:true }).json({
             _id: user._id,
             username: user.username,
             fullName: user.fullName,
