45ed38b1ccd939fcf2b812478127997c912d6393
SARTH NAROLA <168976016+Sarth00718@users.noreply.github.com>
2025-07-13 16:42:57 +0530

    Update socket.js

--- backend/socket/socket.js
+++ backend/socket/socket.js
@@ -7,8 +7,8 @@ const app = express();
 const server = http.createServer(app);
 const io = new Server(server, {
     cors:{
-       origin:['http://localhost:5173'],
-       // origin: ['https://chat-application-1-eyqu.onrender.com'],
+       //origin:['http://localhost:5173'],
+        origin: ['https://real-time-chat-application-front.onrender.com'],
         methods:['GET', 'POST'],
     },
 });
