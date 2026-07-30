b5d15d763f0ac10fb9566f75e9a4bae10f9c8325
Sarth00718 <sarthnarola007@gmail.com>
2026-07-30 14:51:38 +0530

    feat: initialize MongoDB connection utility with connection event handling

--- backend/config/db.js
+++ backend/config/db.js
@@ -4,7 +4,11 @@ import { DB_NAME } from "../constants.js";
 const connectDB = async () => {
     try {
         const connectionInstance = await mongoose.connect(process.env.MONGODB_URI, {
-            dbName: DB_NAME
+            dbName: DB_NAME,
+            serverSelectionTimeoutMS: 5000, // Keep trying to send operations for 5 seconds
+            socketTimeoutMS: 45000, // Close sockets after 45 seconds of inactivity
+            maxIdleTimeMS: 60000, // Close idle connections after 60 seconds
+            family: 4 // Use IPv4, skip trying IPv6
         });
         console.log(`\nMongoDB connected !! DB HOST: ${connectionInstance.connection.host}`);
     } catch (error) {
